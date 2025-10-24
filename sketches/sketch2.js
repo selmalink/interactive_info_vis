8b72fc08243ca8ccc994c008ff6977f871b53c92
selmalink <selmalink@gmail.com>
2025-10-24 12:50:06 -0700

    Map minute() to cake layers for Cake Clock

--- sketches/sketch2.js
+++ sketches/sketch2.js
@@ -10,16 +10,62 @@ registerSketch('sk2', function (p) {
   p.draw = function () {
     p.background(255);
 
-    let hr24 = p.hour();
     let mn = p.minute();
-    let sc = p.second();
+
+    let cakeCenterX = p.width / 2;
+    let cakeBaseY   = p.height * 0.65;
+    let cakeWidth = p.width * 0.45; 
+    let cakeHeightPerLayer = 12;
+
+    let maxLayers = 10;
+    let numLayers = p.floor(p.map(mn, 0, 59, 1, maxLayers + 1));
+   
+    p.noStroke();
+    p.fill(230); 
+    p.ellipse(
+      cakeCenterX,
+      cakeBaseY + 20,
+      cakeWidth * 1.3,
+      30
+    );
+
+    p.rectMode(p.CENTER);
+    for (let i = 0; i < numLayers; i++) {
+      let layerY = cakeBaseY - i * cakeHeightPerLayer;
+
+      if (i % 2 === 0) {
+        p.fill(255);   
+      } else {
+        p.fill(200);   
+      }
+
+      p.rect(
+        cakeCenterX,
+        layerY,
+        cakeWidth,
+        cakeHeightPerLayer + 2,
+        4 
+      );
+    }
+
+    let topY = cakeBaseY - (numLayers - 1) * cakeHeightPerLayer - cakeHeightPerLayer / 2;
+
+    p.fill(255);
+    p.rect(
+      cakeCenterX,
+      topY,
+      cakeWidth,
+      cakeHeightPerLayer + 4,
+      6 
+    );
 
     p.fill(0);
-    p.textSize(16);
+    p.textAlign(p.CENTER);
+    p.textSize(14);
     p.text(
-      "hour: " + hr24 + "   min: " + mn + "   sec: " + sc,
+      "Minute → number of cake layers\nCurrent minute: " + mn,
       p.width / 2,
-      p.height / 2
+      40
     );
   };
 
@@ -27,3 +73,5 @@ registerSketch('sk2', function (p) {
     p.resizeCanvas(p.windowWidth, p.windowHeight);
   };
 });
+
+
