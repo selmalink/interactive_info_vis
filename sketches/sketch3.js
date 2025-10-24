9b4f5caa99b28be314eb4430b694196b5eefbd56
selmalink <selmalink@gmail.com>
2025-10-24 14:50:54 -0700

    add Heatwave Clock base sketch with time readout git push

--- sketches/sketch3.js
+++ sketches/sketch3.js
@@ -1,14 +1,30 @@
 // Instance-mode sketch for tab 3
 registerSketch('sk3', function (p) {
+
   p.setup = function () {
     p.createCanvas(p.windowWidth, p.windowHeight);
-  };
-  p.draw = function () {
-    p.background(240, 200, 200);
-    p.fill(180, 60, 60);
-    p.textSize(32);
+    p.noStroke();
     p.textAlign(p.CENTER, p.CENTER);
-    p.text('HWK #4. B', p.width / 2, p.height / 2);
+    p.rectMode(p.CENTER);
   };
-  p.windowResized = function () { p.resizeCanvas(p.windowWidth, p.windowHeight); };
+
+  p.draw = function () {
+    p.background(0); 
+
+    let hr24 = p.hour();   // 0-23
+    let mn   = p.minute(); // 0-59
+    let sc   = p.second(); // 0-59
+
+    p.fill(255);
+    p.textSize(16);
+    p.text(
+      "Heatwave Clock base\nhour: " + hr24 + "  min: " + mn + "  sec: " + sc,
+      p.width / 2,
+      p.height / 2
+    );
+  };
+
+  p.windowResized = function () {
+    p.resizeCanvas(p.windowWidth, p.windowHeight);
+  };
 });
