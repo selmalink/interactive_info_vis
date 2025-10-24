18241fe4661168cc32c22aa01857488c99b38c81
selmalink <selmalink@gmail.com>
2025-10-24 12:31:05 -0700

    feat: add p5 instance skeleton and canvas for Cake Clock

--- sketches/sketch2.js
+++ sketches/sketch2.js
@@ -1,14 +1,29 @@
 // Instance-mode sketch for tab 2
 registerSketch('sk2', function (p) {
+
   p.setup = function () {
     p.createCanvas(p.windowWidth, p.windowHeight);
+    p.noStroke();
+    p.textAlign(p.CENTER);
   };
+
   p.draw = function () {
-    p.background(220);
-    p.fill(100, 150, 240);
-    p.textSize(32);
-    p.textAlign(p.CENTER, p.CENTER);
-    p.text('HWK #4. A', p.width / 2, p.height / 2);
+    p.background(255);
+
+    let hr24 = p.hour();
+    let mn = p.minute();
+    let sc = p.second();
+
+    p.fill(0);
+    p.textSize(16);
+    p.text(
+      "hour: " + hr24 + "   min: " + mn + "   sec: " + sc,
+      p.width / 2,
+      p.height / 2
+    );
   };
-  p.windowResized = function () { p.resizeCanvas(p.windowWidth, p.windowHeight); };
+
+  p.windowResized = function () {
+    p.resizeCanvas(p.windowWidth, p.windowHeight);
+  };
 });
